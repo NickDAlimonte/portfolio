b7862ff8a635c804a7e287313f2cc76d4c2471bf
Nickolas D'Alimonte <ND135@myscc.ca>
2026-06-17 17:25:53 -0400

    Finished the framework for the calendar. moving towards building dynamically added holidays.

--- src/app/calendar-app/calendar-app.ts
+++ src/app/calendar-app/calendar-app.ts
@@ -7,5 +7,66 @@ import { Component } from '@angular/core';
   styleUrl: './calendar-app.scss',
 })
 export class CalendarApp {
+  currentDate: Date = new Date();
+  selectedYear = 0;
+  selectedMonth = ""
 
+  monthList = Array.from({length: 12}, (_,i) => i);
+
+  yearsList = Array.from({ length: 100}, (_,i) => this.currentDate.getFullYear() - 50 + i);
+
+  ngOnInit(){
+    this.selectedYear = this.currentDate.getFullYear();
+    this.selectedMonth = this.currentDate.toLocaleString('default', {month: 'long'})
+
+  }
+
+  selectYear(number: number) {
+    this.currentDate.setFullYear(number)
+    this.selectedYear = this.currentDate.getFullYear();
+  }
+
+  selectMonth(number: number){
+    this.currentDate.setMonth(number)
+    this.selectedMonth = this.currentDate.toLocaleString('default', {month: 'long'});
+    console.log(this.getWeekDateArray())
+  }
+
+  getMonthName(month: number): string{
+    return new Date(this.selectedYear, month, 1).toLocaleString('default', {month: 'long'})
+  }
+
+  getMonthinDays(monthOffset: number = 1, dateOffset: number = 0){
+    return new Date(this.selectedYear, this.currentDate.getMonth()+monthOffset, 0).getDate()-dateOffset
+  }
+
+  getWeekDateArray(){
+    const weekDate: number[][] = []
+    const daysOfMonth = this.getMonthinDays();
+    const weekday = new Date(this.selectedYear, this.currentDate.getMonth()).getDay();
+    let week: number[] = []
+
+    if(weekday !== 0){
+      for(let daysAhead = 0; daysAhead < weekday; daysAhead++){
+        week.push(this.getMonthinDays(0, weekday-1-daysAhead))
+      }
+    }
+
+    for (let day = 1; day <= daysOfMonth; day++){
+      week.push(day);
+
+      if (week.length === 7){
+        weekDate.push(week);
+        week = [];
+      }
+    }
+
+    if(week.length > 0){
+      weekDate.push(week);
+    }
+
+    
+
+    return weekDate;
+  }
 }
